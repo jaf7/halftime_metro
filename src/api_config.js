046c6eb4a868a3d4098916e7a9462bfe75be341d
jaf7 <janthonyfields@gmail.com>
2018-07-19 18:21:19 -0400

    update production hostname in api_config.js

--- src/api_config.js
+++ src/api_config.js
@@ -1,7 +1,7 @@
 let backendHost
 const hostname = window && window.location && window.location.hostname
 
-if ( hostname === 'halftime-metro-frontend.herokuapp.com' ) {
+if ( hostname === 'jaf7.github.io' ) {
   backendHost = 'https://halftime-metro-backend.herokuapp.com'
 } else if ( hostname === '127.0.0.1' ) {
   backendHost = 'http://localhost:3001'
