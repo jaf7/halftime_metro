74032c81ceb8acacda35a2be51316f24b55a0703
jaf7 <janthonyfields@gmail.com>
2018-07-19 17:55:54 -0400

    Credentialed maps API access

---
+++ src/api_key.js
@@ -0,0 +1,6 @@
+const API_KEY = "AIzaSyD7yUUJjuy7n4GbG-sDK1qJh3zfTMthgpE"
+
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7yUUJjuy7n4GbG-sDK1qJh3zfTMthgpE&libraries=places&callback=initMap" async defer></script>
+// <!-- Load maps API asynchronously, then call initMap() -->
+// <script src="https://maps.googleapis.com/maps/api/js?key=`${API_KEY}`&libraries=places&callback=initMap" async defer></script>
+
